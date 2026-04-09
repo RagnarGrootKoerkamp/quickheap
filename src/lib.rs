e508609e98d040017105105e1cec1e778a9df439
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-04-09 15:50:15 +0200

    test more SimdQuickHeap params

--- src/lib.rs
+++ src/lib.rs
@@ -84,10 +84,16 @@ mod test {
 
         #[cfg(feature = "avx2")]
         TestHeap::<T, Base, SimdQuickHeap<T, Avx2, 8, 3>>::run(n);
+        #[cfg(feature = "avx2")]
+        TestHeap::<T, Base, SimdQuickHeap<T, Avx2, 16, 1>>::run(n);
         #[cfg(feature = "avx512")]
         TestHeap::<T, Base, SimdQuickHeap<T, Avx512<false>, 8, 3>>::run(n);
         #[cfg(feature = "avx512")]
+        TestHeap::<T, Base, SimdQuickHeap<T, Avx512<false>, 16, 1>>::run(n);
+        #[cfg(feature = "avx512")]
         TestHeap::<T, Base, SimdQuickHeap<T, Avx512<true>, 8, 3>>::run(n);
+        #[cfg(feature = "avx512")]
+        TestHeap::<T, Base, SimdQuickHeap<T, Avx512<true>, 16, 1>>::run(n);
 
         #[cfg(feature = "ffi")]
         TestHeap::<T, Base, sequence_heap::SequenceHeapI64>::run(n);
