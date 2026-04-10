bbb27c61e357b20b7ebc7ecbda5ab4f1ac995282
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-04-10 20:39:20 +0200

    S3Q and SequenceHeap offset data by 1 to avoid issues with inserting 0

--- src/s3q.rs
+++ src/s3q.rs
@@ -1,12 +1,12 @@
+use crate::Heap;
 use crate::impls::NoHeap;
 use crate::workloads::Elem;
-use crate::Heap;
 use s3q_sys::{
-    s3q_i32_pq_empty, s3q_i32_pq_free, s3q_i32_pq_new, s3q_i32_pq_pop, s3q_i32_pq_push,
-    s3q_i64_pq_empty, s3q_i64_pq_free, s3q_i64_pq_new, s3q_i64_pq_pop, s3q_i64_pq_push,
-    s3q_u32_pq_empty, s3q_u32_pq_free, s3q_u32_pq_new, s3q_u32_pq_pop, s3q_u32_pq_push,
-    s3q_u64_pq_empty, s3q_u64_pq_free, s3q_u64_pq_new, s3q_u64_pq_pop, s3q_u64_pq_push,
-    S3qI32Pq, S3qI64Pq, S3qU32Pq, S3qU64Pq,
+    S3qI32Pq, S3qI64Pq, S3qU32Pq, S3qU64Pq, s3q_i32_pq_empty, s3q_i32_pq_free, s3q_i32_pq_new,
+    s3q_i32_pq_pop, s3q_i32_pq_push, s3q_i64_pq_empty, s3q_i64_pq_free, s3q_i64_pq_new,
+    s3q_i64_pq_pop, s3q_i64_pq_push, s3q_u32_pq_empty, s3q_u32_pq_free, s3q_u32_pq_new,
+    s3q_u32_pq_pop, s3q_u32_pq_push, s3q_u64_pq_empty, s3q_u64_pq_free, s3q_u64_pq_new,
+    s3q_u64_pq_pop, s3q_u64_pq_push,
 };
 
 macro_rules! impl_s3q_heap {
@@ -25,13 +25,16 @@ macro_rules! impl_s3q_heap {
             #[inline(always)]
             fn default() -> Self {
                 let pq = unsafe { $new() };
-                assert!(!pq.is_null(), concat!(stringify!($new), ": allocation failed"));
+                assert!(
+                    !pq.is_null(),
+                    concat!(stringify!($new), ": allocation failed")
+                );
                 $heap(pq)
             }
 
             #[inline(always)]
             fn push(&mut self, t: $t) {
-                unsafe { $push(self.0, t) }
+                unsafe { $push(self.0, t + 1) }
             }
 
             #[inline(always)]
@@ -40,7 +43,7 @@ macro_rules! impl_s3q_heap {
                     if $empty(self.0) {
                         None
                     } else {
-                        Some($pop(self.0))
+                        Some($pop(self.0) - 1)
                     }
                 }
             }
@@ -48,7 +51,43 @@ macro_rules! impl_s3q_heap {
     };
 }
 
-impl_s3q_heap!(S3qHeapI32, i32, S3qI32Pq, s3q_i32_pq_new, s3q_i32_pq_free, s3q_i32_pq_push, s3q_i32_pq_pop, s3q_i32_pq_empty);
-impl_s3q_heap!(S3qHeapI64, i64, S3qI64Pq, s3q_i64_pq_new, s3q_i64_pq_free, s3q_i64_pq_push, s3q_i64_pq_pop, s3q_i64_pq_empty);
-impl_s3q_heap!(S3qHeapU32, u32, S3qU32Pq, s3q_u32_pq_new, s3q_u32_pq_free, s3q_u32_pq_push, s3q_u32_pq_pop, s3q_u32_pq_empty);
-impl_s3q_heap!(S3qHeapU64, u64, S3qU64Pq, s3q_u64_pq_new, s3q_u64_pq_free, s3q_u64_pq_push, s3q_u64_pq_pop, s3q_u64_pq_empty);
+impl_s3q_heap!(
+    S3qHeapI32,
+    i32,
+    S3qI32Pq,
+    s3q_i32_pq_new,
+    s3q_i32_pq_free,
+    s3q_i32_pq_push,
+    s3q_i32_pq_pop,
+    s3q_i32_pq_empty
+);
+impl_s3q_heap!(
+    S3qHeapI64,
+    i64,
+    S3qI64Pq,
+    s3q_i64_pq_new,
+    s3q_i64_pq_free,
+    s3q_i64_pq_push,
+    s3q_i64_pq_pop,
+    s3q_i64_pq_empty
+);
+impl_s3q_heap!(
+    S3qHeapU32,
+    u32,
+    S3qU32Pq,
+    s3q_u32_pq_new,
+    s3q_u32_pq_free,
+    s3q_u32_pq_push,
+    s3q_u32_pq_pop,
+    s3q_u32_pq_empty
+);
+impl_s3q_heap!(
+    S3qHeapU64,
+    u64,
+    S3qU64Pq,
+    s3q_u64_pq_new,
+    s3q_u64_pq_free,
+    s3q_u64_pq_push,
+    s3q_u64_pq_pop,
+    s3q_u64_pq_empty
+);
--- src/sequence_heap.rs
+++ src/sequence_heap.rs
@@ -1,12 +1,12 @@
+use crate::Heap;
 use crate::impls::NoHeap;
 use crate::workloads::Elem;
-use crate::Heap;
 use sequence_heap_sys::{
-    seq_heap_i32_empty, seq_heap_i32_free, seq_heap_i32_new, seq_heap_i32_pop, seq_heap_i32_push,
-    seq_heap_i64_empty, seq_heap_i64_free, seq_heap_i64_new, seq_heap_i64_pop, seq_heap_i64_push,
-    seq_heap_u32_empty, seq_heap_u32_free, seq_heap_u32_new, seq_heap_u32_pop, seq_heap_u32_push,
-    seq_heap_u64_empty, seq_heap_u64_free, seq_heap_u64_new, seq_heap_u64_pop, seq_heap_u64_push,
-    SeqHeapI32, SeqHeapI64, SeqHeapU32, SeqHeapU64,
+    SeqHeapI32, SeqHeapI64, SeqHeapU32, SeqHeapU64, seq_heap_i32_empty, seq_heap_i32_free,
+    seq_heap_i32_new, seq_heap_i32_pop, seq_heap_i32_push, seq_heap_i64_empty, seq_heap_i64_free,
+    seq_heap_i64_new, seq_heap_i64_pop, seq_heap_i64_push, seq_heap_u32_empty, seq_heap_u32_free,
+    seq_heap_u32_new, seq_heap_u32_pop, seq_heap_u32_push, seq_heap_u64_empty, seq_heap_u64_free,
+    seq_heap_u64_new, seq_heap_u64_pop, seq_heap_u64_push,
 };
 
 macro_rules! impl_sequence_heap {
@@ -25,13 +25,16 @@ macro_rules! impl_sequence_heap {
             #[inline(always)]
             fn default() -> Self {
                 let pq = unsafe { $new() };
-                assert!(!pq.is_null(), concat!(stringify!($new), ": allocation failed"));
+                assert!(
+                    !pq.is_null(),
+                    concat!(stringify!($new), ": allocation failed")
+                );
                 $heap(pq)
             }
 
             #[inline(always)]
             fn push(&mut self, t: $t) {
-                unsafe { $push(self.0, t) }
+                unsafe { $push(self.0, t + 1) }
             }
 
             #[inline(always)]
@@ -40,7 +43,7 @@ macro_rules! impl_sequence_heap {
                     if $empty(self.0) {
                         None
                     } else {
-                        Some($pop(self.0))
+                        Some($pop(self.0) - 1)
                     }
                 }
             }
@@ -48,7 +51,43 @@ macro_rules! impl_sequence_heap {
     };
 }
 
-impl_sequence_heap!(SequenceHeapI32, i32, SeqHeapI32, seq_heap_i32_new, seq_heap_i32_free, seq_heap_i32_push, seq_heap_i32_pop, seq_heap_i32_empty);
-impl_sequence_heap!(SequenceHeapI64, i64, SeqHeapI64, seq_heap_i64_new, seq_heap_i64_free, seq_heap_i64_push, seq_heap_i64_pop, seq_heap_i64_empty);
-impl_sequence_heap!(SequenceHeapU32, u32, SeqHeapU32, seq_heap_u32_new, seq_heap_u32_free, seq_heap_u32_push, seq_heap_u32_pop, seq_heap_u32_empty);
-impl_sequence_heap!(SequenceHeapU64, u64, SeqHeapU64, seq_heap_u64_new, seq_heap_u64_free, seq_heap_u64_push, seq_heap_u64_pop, seq_heap_u64_empty);
+impl_sequence_heap!(
+    SequenceHeapI32,
+    i32,
+    SeqHeapI32,
+    seq_heap_i32_new,
+    seq_heap_i32_free,
+    seq_heap_i32_push,
+    seq_heap_i32_pop,
+    seq_heap_i32_empty
+);
+impl_sequence_heap!(
+    SequenceHeapI64,
+    i64,
+    SeqHeapI64,
+    seq_heap_i64_new,
+    seq_heap_i64_free,
+    seq_heap_i64_push,
+    seq_heap_i64_pop,
+    seq_heap_i64_empty
+);
+impl_sequence_heap!(
+    SequenceHeapU32,
+    u32,
+    SeqHeapU32,
+    seq_heap_u32_new,
+    seq_heap_u32_free,
+    seq_heap_u32_push,
+    seq_heap_u32_pop,
+    seq_heap_u32_empty
+);
+impl_sequence_heap!(
+    SequenceHeapU64,
+    u64,
+    SeqHeapU64,
+    seq_heap_u64_new,
+    seq_heap_u64_free,
+    seq_heap_u64_push,
+    seq_heap_u64_pop,
+    seq_heap_u64_empty
+);
