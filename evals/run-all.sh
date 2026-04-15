981285cd3bf5780db952bebd4248d0c3d0775d66
Johannes Breitling <mail@johannesbreitling.de>
2026-04-15 17:19:26 +0200

    add runner file for server batch jobs

---
+++ evals/run-all.sh
@@ -0,0 +1,3 @@
+cargo run -r --example bench -F avx512 > ./test-diffie.csv
+cargo run -r --example bench -F avx512 -- --comparisons > ./test-comparisons.csv
+cargo run -r --example bench_graph -F avx512 > ./test-graphs-diffie.csv
