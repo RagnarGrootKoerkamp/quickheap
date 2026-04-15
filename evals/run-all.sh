0c75e7286dccfd360acf2d21c7590bc33cb2562a
Johannes Breitling <mail@johannesbreitling.de>
2026-04-15 17:27:16 +0200

    fix for runner script

--- evals/run-all.sh
+++ evals/run-all.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cargo run -r --example bench -F avx512 > ./test-diffie.csv
 cargo run -r --example bench -F avx512 -- --comparisons > ./test-comparisons.csv
 cargo run -r --example bench_graph -F avx512 > ./test-graphs-diffie.csv
