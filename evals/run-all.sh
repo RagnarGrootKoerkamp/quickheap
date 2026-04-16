5f2987f9df1b21031f3661914dccea120b1af702
Johannes Breitling <mail@johannesbreitling.de>
2026-04-16 14:57:00 +0200

    rerun experiments with new wiggles

--- evals/run-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cargo run -r --example bench -F avx512 > ./test-diffie.csv
-cargo run -r --example bench -F avx512 -- --comparisons > ./test-comparisons.csv
-cargo run -r --example bench_graph -F avx512 > ./test-graphs-diffie.csv
---
+++ run-all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# cargo run -r --example bench -F avx512 > ./evals/test-diffie.csv
+# cargo run -r --example bench -F avx512 -- --comparisons > ./evals/test-comparisons.csv
+cargo run -r --example bench_graph -F avx512 > ./evals/test-graphs-diffie.csv
