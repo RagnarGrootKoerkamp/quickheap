2cd57aa269b95028616ab2e1574810e864fdd850
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-06-30 20:48:14 +0200

    sort run-all by fast -> slow

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cargo run -r --package bench --example bench -F avx512 >./bench/evals/data/results-plot.csv
-cargo run -r --package bench --example bench -F avx512,perf,boost -- --table >./bench/evals/data/results-table.csv
-cargo run -r --package bench --example bench -F avx512 -- --comparisons >./bench/evals/data/results-comparisons.csv
-cargo run -r --package bench --example bench -F avx512 -- --ablation >./bench/evals/data/results-ablation.csv
 cargo run -r --package bench --example bench_graph -F avx512 >./bench/evals/data/results-graphs.csv
+cargo run -r --package bench --example bench -F avx512 -- --ablation >./bench/evals/data/results-ablation.csv
+cargo run -r --package bench --example bench -F avx512 -- --comparisons >./bench/evals/data/results-comparisons.csv
+cargo run -r --package bench --example bench -F avx512,perf,boost -- --table >./bench/evals/data/results-table.csv
+cargo run -r --package bench --example bench -F avx512 >./bench/evals/data/results-plot.csv
